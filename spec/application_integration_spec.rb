febb7975ccf39e14457396f5c27c92207a8a9f2b
Heather Kirk <hskirk@gmail.com>
2019-04-18 01:53:01 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -30,6 +30,14 @@ describe "GET '/' - Greeting Form" do
   it 'welcomes the user' do 
     visit '/'
     expect(page.body).to include("Welcome!")
-  end 
-  end 
+    end 
+  
+  it 'has a greeting form with a user_name field' do 
+    visit '/'
+    
+    expect(page).to have_selector("form")
+    expect(page).to have_field(:user_name)
+    end 
+end 
+  
 end
